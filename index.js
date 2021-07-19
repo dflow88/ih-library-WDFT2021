824152d775a2a4306e569485ed94b21fee2a0fd5
¨Diego <dieflope@gmail.com>
2021-07-19 11:22:08 -0500

    Crud terminado

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express       = require('express')
 const app           = express()      
 const mongoose      = require("mongoose")
+const bodyParser    = require("body-parser")
+
 const Book          = require('./models/Book')
+
 // 2. MIDDLEWARES
 require('dotenv').config()
 mongoose.connect(process.env.MONGODB, {
@@ -12,12 +15,29 @@ mongoose.connect(process.env.MONGODB, {
 })
     .then(() => {console.log("Conectados a MongoDB")})
     .catch((e) => console.log(e))
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'))
 app.set("view engine", "hbs")
 // 3. RUTAS
 app.get("/", (req,res) => {
     res.render("index")
 })
+
+
+app.get("/books/create", (req, res) => {
+    res.render("book-create")
+})
+
+app.post("/books/create", (req, res) => {
+    const { title, author, description, rating } = req.body
+    Book.create({title, author, description, rating})
+        .then((libroCreado) => {
+            res.redirect("/books")
+        })
+        .catch(e => console.log(e))
+})
+
 app.get("/books", (req, res) => {
     Book.find({})
         .then((librosEncontrados) => {
@@ -43,6 +63,60 @@ app.get("/books/:bookId", (req, res) => {
       })
       .catch(e => console.log(e))
 })
+
+app.get("/search", (req, res) => {
+
+    const queries = req.query
+
+    res.render("search", {
+        busqueda: queries
+    })
+})
+
+app.post("/search", (req, res) => {
+    const valorFormulario = req.body
+    
+    res.redirect(`/search?palabra=${valorFormulario.palabra}&nombre=${valorFormulario.nombre}&apellido=${valorFormulario.apellido}`)
+
+})
+
+app.get('/books/:bookId/edit', (req, res) => {
+    const { bookId} = req.params
+
+    Book.findById(bookId)
+        .then(libroEncontrado => {
+            res.render("book-edit", {
+                libro: libroEncontrado
+            })
+            console.log(libroEncontrado)
+        })
+        .catch((e) => console.log(e))
+})
+
+app.post("/books/:bookId/edit", (req, res) => {
+    // PARÁMETROS DE LA URL (bookId)
+    const { bookId } = req.params
+    // DATOS DEL FORMULARIO
+    const { title, description, author, rating } = req.body
+    Book.findByIdAndUpdate(bookId, {title, description, author, rating}, {new: true})
+        .then(libroActualizado => {
+            console.log(libroActualizado)
+            res.redirect(`/books/${libroActualizado.id}`)
+        })
+        .catch(e => console.log(e))    
+})
+
+app.post("/books/:bookId/delete", (req, res) => {
+    const { bookId } = req.params
+    
+    Book.findByIdAndDelete(bookId)
+        .then(() => {
+            res.redirect("/books")
+        }
+        )
+        .catch(e => console.log(e))
+})
+
 
 // 4. SERVIDOR
 app.listen(process.env.PORT, () => {
